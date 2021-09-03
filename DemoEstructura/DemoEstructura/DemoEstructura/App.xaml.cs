751cc28c2c58574b818b3d9d00a9d49419997e28
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2021-09-03 20:24:57 +0200

    Demo Estructura Solucion Xamarin Forms

---
+++ DemoEstructura/DemoEstructura/DemoEstructura/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoEstructura
+{
+    public partial class App : Application
+    {
+        public static string Mensaje;
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new PaginaInicial();
+        }
+
+        protected override void OnStart()
+        {
+            Mensaje = "Aplicacion iniciada";
+        }
+
+        protected override void OnSleep()
+        {
+            
+        }
+
+        protected override void OnResume()
+        {
+            Mensaje = "Aplicacion restaurada";
+        }
+    }
+}
---
+++ DemoEstructura/DemoEstructura/DemoEstructura/PaginaInicial.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoEstructura
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PaginaInicial : ContentPage
+    {
+        public PaginaInicial()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Mensaje", App.Mensaje, "OK");
+        }
+
+        private void btnAgregarHora_Clicked(object sender, EventArgs e)
+        {
+            Label texto = new Label();
+            texto.Text = DateTime.Now.TimeOfDay.ToString();
+            texto.TextColor = Color.Blue;
+            texto.FontSize = 15;
+
+            stack.Children.Add(texto);
+        }
+
+        private void btnLimpiar_Clicked(object sender, EventArgs e)
+        {
+            // coleccion de labels agregados
+            var textos = stack.Children.Where(x => x is Label).ToList();
+
+            foreach (var item in textos)
+            {
+                stack.Children.Remove(item);
+            }
+
+        }
+    }
+}
