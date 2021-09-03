907877e2bc9d97fb1777395d2c70c6d4cdf890ac
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2021-09-03 20:24:19 +0200

    Demo01 - Paginas en Xamarin Forms

---
+++ Demo01_Paginas/Demo01_Paginas/Demo01_Paginas/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo01_Paginas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Paginas.PaginaLogin();
+                //new NavigationPage(new Paginas.PaginaLogin());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Demo01_Paginas/Demo01_Paginas/Demo01_Paginas/Paginas/PaginaLogin.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo01_Paginas.Paginas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PaginaLogin : ContentPage
+    {
+        public PaginaLogin()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (entryClave.Text == "123" && entryPassword.Text == "pass")
+            {
+                await DisplayAlert("Bienvenido", "Ingresando al sistema...", "OK");
+                App.Current.MainPage = new NavigationPage(new PaginaProducto());
+
+                //await Navigation.PushAsync(new PaginaProducto());
+            }
+            else
+            {
+                await DisplayAlert("Error", "Datos no válidos...", "OK");
+            }
+        }
+    }
+}
