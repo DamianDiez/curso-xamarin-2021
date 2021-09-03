dc5b7abeeaddd5405a309972ca0e70fe373f0cab
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2021-09-03 20:26:17 +0200

    Demo Controles Basicos

---
+++ Demo02_Controles/Demo02_Controles/Demo02_Controles/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo02_Controles
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Paginas.PaginaDatos();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
