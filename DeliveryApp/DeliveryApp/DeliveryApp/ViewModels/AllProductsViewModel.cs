488b89facea7dc3ee28b0e05c668e785124d74ff
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2021-09-11 15:57:44 +0200

    DeliveryApp - CarouselView and minor fixes in CollectionView

--- DeliveryApp/DeliveryApp/DeliveryApp/ViewModels/AllProductsViewModel.cs
+++ DeliveryApp/DeliveryApp/DeliveryApp/ViewModels/AllProductsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using DeliveryApp.Models;
 using DeliveryApp.Services;
+using DeliveryApp.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -16,17 +17,74 @@ namespace DeliveryApp.ViewModels
 
         public ObservableCollection<Product> ProductCollection { get; set; }
 
-        public ICommand LoadDataCommand { get; set; }
+        private Product selectedProduct;
+
+        public Product SelectedProduct
+        {
+            get { return selectedProduct; }
+            set { SetProperty(ref selectedProduct, value); }
+        }
+
+        public ICommand LoadDataCommand { get; private set; }
+        public ICommand GoToProductDetailCommand { get; private set; }
+        public ICommand LoadMoreCommand { get; private set; }
+
+        private int productsPerPage = 10;
+        private int page = 0;
 
         private async Task LoadData()
         {
+            if (ProductCollection.Count == 0)
+            {
+                IsBusy = true;
+
+                await Task.Delay(4000);
+                products = HardCodeService.GetProductsPerPage(page, productsPerPage);
+                SetProductCollection(products);
+
+                IsBusy = false;
+            }
+        }
+
+        private async Task LoadMore()
+        {
+            if (IsBusy)
+                return;
+
             IsBusy = true;
 
-            await Task.Delay(4000);
-            products = HardCodeService.GetAllProducts();
-            SetProductCollection(products);
+            await Task.Delay(2000);
+            page++;
+            var moreProducts = HardCodeService.GetProductsPerPage(page, productsPerPage);
+
+            if (moreProducts?.Count > 0)
+            {
+                products.AddRange(moreProducts);
+
+                foreach (var item in moreProducts)
+                {
+                    ProductCollection.Add(item);
+                }
+            }
+            else
+            {
+                page--;
+            }
 
             IsBusy = false;
+        }
+
+        private async Task GoToDetail()
+        {
+            if (selectedProduct != null)
+            {
+                ProductDetailViewModel vm = new ProductDetailViewModel(selectedProduct);
+
+                ProductDetailView page = new ProductDetailView();
+                page.BindingContext = vm;
+
+                await App.Current.MainPage.Navigation.PushAsync(page); 
+            }
         }
 
         private void SetProductCollection(List<Product> products)
@@ -41,8 +99,11 @@ namespace DeliveryApp.ViewModels
 
         public AllProductsViewModel()
         {
+            SelectedProduct = null;
             ProductCollection = new ObservableCollection<Product>();
             LoadDataCommand = new Command(async () => await LoadData());
+            LoadMoreCommand = new Command(async () => await LoadMore());
+            GoToProductDetailCommand = new Command(async () => await GoToDetail());
         }
     }
 }
